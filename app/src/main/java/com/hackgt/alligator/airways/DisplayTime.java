dc1f287489291432bff3e43d9b52f2528416d415
mjkrumwiede <mjkrumwiede@gmail.com>
2016-09-25 04:27:20 -0400

    added GPS capabilities

--- app/src/main/java/com/hackgt/alligator/airways/DisplayTime.java
+++ app/src/main/java/com/hackgt/alligator/airways/DisplayTime.java
@@ -1,8 +1,17 @@
 package com.hackgt.alligator.airways;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,12 +39,19 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 
-public class DisplayTime extends AppCompatActivity {
+public class DisplayTime extends AppCompatActivity{
     private static String message;
     private String airportCode;
     private final static String apikey = "WISwm1hTrfWfZGTDxULy1csrxNQddEd4";
     private final static String baseUrl = "https://demo30-test.apigee.net/v1/hack/";
     private String terminal;
+    private String airportLat;
+    private String airportLong;
+    private String myLat;
+    private String myLong;
+    private Integer hoursToAirport;
+    private Integer minToAirport;
+    private Integer secToAirport;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -49,13 +65,39 @@ public class DisplayTime extends AppCompatActivity {
 
         Intent intent = getIntent();
         message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+        getPermissions();
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_LOW);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        String provider=locationManager.getBestProvider(criteria, true);
+        if (provider == null) {
+            Log.i("INFO", "Provider is null");
+            return;
+        } else {
+            Log.i("INFO", "Provider: " + provider);
+        }
+        MyCurrentLocationListener locationListener = new MyCurrentLocationListener();
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            Log.i("INFO","You're in!");
+        }catch(SecurityException e){
+            Log.i("INFO","Didn't Work!");
+        }
+
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
         AsyncTask task = new RetrieveFeedTask().execute();
 //        if (task.getStatus().equals(AsyncTask.Status.FINISHED)) {
             new TsaTask().execute();
 //        }
+        new GoogleTask().execute();
 
     }
 
@@ -120,7 +162,7 @@ public class DisplayTime extends AppCompatActivity {
             try {
                 URL url = new URL(baseUrl + "status?flightNumber=" + message + "&flightOriginDate=" +
                         formattedDate + "&apikey=" + apikey);
-                urlString = url.toString();
+
 
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
@@ -157,6 +199,8 @@ public class DisplayTime extends AppCompatActivity {
                 check = checkArr.getJSONObject(1);
                 String test = check.getString("departureAirportName");
                 airportCode = check.getString("departureAirportCode");
+                airportLat = check.getString("arrivalTsoagLatitudeDecimal");
+                airportLong = check.getString("arrivalTsoagLongitudeDecimal");
                 terminal = check.getString("departureTerminal");
                 switch (terminal) {
                     case "Domestic Term-South":
@@ -191,6 +235,7 @@ public class DisplayTime extends AppCompatActivity {
             try {
                 URL url = new URL(baseUrl + "tsa?airport=" + "ATL" + "&apikey=" + apikey);
                 urlString = url.toString();
+                Log.i("INFO",urlString);
 
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
@@ -249,6 +294,155 @@ public class DisplayTime extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+        }
+    }
+
+    class GoogleTask extends AsyncTask<Void, Void, String> {
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        TextView responseView = (TextView) findViewById(R.id.responseView);
+        String urlString;
+        String baseUrl = "https://maps.googleapis.com/maps/api/directions/json?";
+        String apikey = "AIzaSyDZl2HiMQF1L9eEwFoCkJ5oHBo5G_hJWvQ";
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+            try {
+                while(myLat==null){
+
+                }
+                URL url = new URL(baseUrl + "origin="+myLat+","+myLong+"&destination="+airportLat+","+airportLong+
+                "&key="+apikey);
+                urlString = url.toString();
+                Log.i("INFO",urlString);
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            //responseView.setText(response);
+            try {
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                JSONArray checkARR = object.getJSONArray("routes");
+                JSONObject check = checkARR.getJSONObject(0);
+                JSONArray checkARR2 = check.getJSONArray("legs");
+                check = checkARR2.getJSONObject(0);
+                int timeToTravel=check.getJSONObject("duration").getInt("value");
+                hoursToAirport = (int)Math.floor(timeToTravel/3600);
+                timeToTravel = timeToTravel % 3600;
+                minToAirport = (int)Math.floor(timeToTravel/60);
+                timeToTravel = timeToTravel % 60;
+                secToAirport = timeToTravel;
+                responseView.setText("You are "+hoursToAirport+" hrs, "+minToAirport+" min, and " +
+                        secToAirport+" sec from Airport!");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class MyCurrentLocationListener implements android.location.LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            myLat = Double.toString(location.getLatitude());
+            myLong = Double.toString(location.getLongitude());
+            Log.i("INFO","DICKS OUT FOR HARAMBE");
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
+
+    public void getPermissions(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
         }
     }
 }
